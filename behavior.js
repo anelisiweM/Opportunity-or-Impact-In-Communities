ded66f82fc338ce85df70eeec1a23ba9406a6c75
anelisiweM <49754836+anelisiweM@users.noreply.github.com>
2021-12-01 11:25:50 +0200

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+//alert('Hello!');
+
+
+thumbnailElement.addEventListener("click", function() {
+ //document.getElementById("smart_thumbnail");
+ 
+var thumbnailElement = document.getElementById("smart_thumbnail");
+thumbnailElement.className;
+  if(thumbnailElement.className == "")
+  {
+  
+		thumbnailElement.className = "small";
+  //alert("I saw you click!");
+  }
+  else
+  {
+	    thumbnailElement.className = "";
+  //alert("I saw you click!");
+  }
+});
+
+  
+});
+
+
+
+
